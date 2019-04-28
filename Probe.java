96c282122db91fc95d9df3b264a14fba5db8e490
Alwan Andika <47649979+yuuoa@users.noreply.github.com>
2019-04-28 19:15:02 +0700

    Add files via upload

---
+++ Probe.java
@@ -0,0 +1,43 @@
+//by Muhammad Alwan Andika
+//1177050066
+
+import static java.lang.System.*;
+
+public class Probe
+{
+	private String name;
+	private String destination;
+	private String organisation;
+	private String type;
+		
+	public Probe (String name, String destination, String organisation, String type)
+	{
+		this.name = name;
+		this.destination = destination;
+		this.organisation = organisation;
+		this.type = type;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+	
+	public String getDestination()
+	{
+		return destination;
+	}
+	
+	public String getOrganisation()
+	{
+		return organisation;
+	}
+	
+	public String getType()
+	{
+		return type;
+	}
+}
+
+//by Muhammad Alwan Andika
+//1177050066
